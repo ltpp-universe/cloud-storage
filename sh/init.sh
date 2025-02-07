18280a3a4883d25509053d3ff044b6e0479551c5
尤雨东 <root@ltpp.vip>
2025-02-07 22:54:42 +0800

    feat:

--- sh/init.sh
+++ sh/init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git remote add jihulab git@jihulab.com:ltpp-universe/cloud-storage.git;
+git remote add gitee git@gitee.com:ltpp-universe/cloud-storage.git;
 git remote add origin git@github.com:ltpp-universe/cloud-storage.git;
