fa2415d630d41de859b24d5b580a6d0b61b196db
尤雨东 <root@ltpp.vip>
2025-02-09 21:37:43 +0800

    n

--- sh/init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-git init;
-git config core.ignorecase false;
-git remote add gitee git@gitee.com:ltpp-universe/cloud-storage.git;
-git remote add origin git@github.com:ltpp-universe/cloud-storage.git;
